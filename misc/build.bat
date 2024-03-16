d862ce89b74d94c4d3c89ec248ef795a4b5588ed
Marco Caamaño <marco.caamano@gmail.com>
2024-03-16 08:41:12 -0600

    small changes to reorg build environment

--- misc/build.bat
+++ misc/build.bat
@@ -3,7 +3,7 @@ REM @echo off
 REM foobar
 REM call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-mkdir ..\build
-pushd ..\build
-cl /Zi ..\code\win32_handmade.c user32.lib
+mkdir z:\handmade_hero\build
+pushd z:\handmade_hero\build
+cl /Zi z:\handmade_hero\code\win32_handmade.c user32.lib
 popd
